66421440d28bda5380678f14c1e25b792564a7be
mmaurier <marco.maurier@mail.wvu.edu>
2016-03-09 15:38:03 -0500

    final working

--- app/src/main/java/com/example/android/guessthecelebrity/MainActivity.java
+++ app/src/main/java/com/example/android/guessthecelebrity/MainActivity.java
@@ -1,3 +1,7 @@
+/*
+ * This app will download celebs pics and names
+ * and create a guessing game
+ */
 package com.example.android.guessthecelebrity;
 
 import android.graphics.Bitmap;
@@ -12,7 +16,9 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -31,7 +37,23 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<String> celebURLs = new ArrayList<String>();
     ArrayList<String> celebNames = new ArrayList<String>();
     int chosenCeleb = 0;
+    int locationOfCorrectAnswer = 0;
+    String[] answers = new String [4];
+
     ImageView imageView;
+    Button button0;
+    Button button1;
+    Button button2;
+    Button button3;
+
+    public void celebChosen(View view) {
+        if (view.getTag().toString().equals(Integer.toString(locationOfCorrectAnswer))) {
+            Toast.makeText(getApplicationContext(), "Correct!", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Wrong! It was " + celebNames.get(chosenCeleb), Toast.LENGTH_LONG).show();
+        }
+        createNewQuestion();
+    }
 
     public class ImageDonwloader extends AsyncTask<String, Void, Bitmap> {
 
@@ -96,13 +118,17 @@ public class MainActivity extends AppCompatActivity {
         });
 
         imageView = (ImageView)findViewById(R.id.imageView);
+        button0 = (Button) findViewById(R.id.button0);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
 
         DownloadTask task = new DownloadTask();
         String result = null;
         try {
             result = task.execute("http://www.posh24.com/celebrities").get();
             String [] splitResult = result.split("<div class=\"sidebarContainer\">");
-            Pattern p = Pattern.compile("src=\"(.*?)\"");
+            Pattern p = Pattern.compile("<img src=\"(.*?)\"");
             Matcher m = p.matcher(splitResult[0]);
             while (m.find()) {
                 celebURLs.add(m.group(1));
@@ -113,19 +139,43 @@ public class MainActivity extends AppCompatActivity {
                 celebNames.add(m.group(1));
             }
 
-            Random random = new Random();
-            chosenCeleb = random.nextInt(celebURLs.size());
-
-            ImageDonwloader imageTask = new ImageDonwloader();
-            Bitmap celebImage;
-            celebImage = imageTask.execute(celebURLs.get(chosenCeleb)).get();
-            imageView.setImageBitmap(celebImage);
-
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
             e.printStackTrace();
         }
+        createNewQuestion();
+    }
+
+    public void createNewQuestion() {
+        Random random = new Random();
+        chosenCeleb = random.nextInt(celebURLs.size());
+
+        ImageDonwloader imageTask = new ImageDonwloader();
+        Bitmap celebImage;
+
+        try {
+            celebImage = imageTask.execute(celebURLs.get(chosenCeleb)).get();
+            imageView.setImageBitmap(celebImage);
+
+            locationOfCorrectAnswer = random.nextInt(4);
+            int incorrectAnswerLocation;
+            for (int i = 0; i < 4; i++) {
+                if (i == locationOfCorrectAnswer) {
+                    answers[i] = celebNames.get(chosenCeleb);
+                } else {
+                    incorrectAnswerLocation = random.nextInt(celebURLs.size());
+                    while (incorrectAnswerLocation == chosenCeleb) {
+                        incorrectAnswerLocation = random.nextInt(celebURLs.size());
+                    }
+                    answers[i] = celebNames.get(incorrectAnswerLocation);
+                }
+            }
+            button0.setText(answers[0]);
+            button1.setText(answers[1]);
+            button2.setText(answers[2]);
+            button3.setText(answers[3]);
+        } catch (Exception e) { e.printStackTrace(); }
     }
 
     @Override
